55a7b88e1a23426e88fcebc605a5e7492eb4954e
Paulina Sophia Gerchuk <paulinagerchuk@paulinas-air-3.lan>
2022-12-26 20:42:49 +0000

    Small tweaks to proportions and extra warning messages

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// adding scroll variable to control css animations
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
