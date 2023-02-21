946bb445b26c60670442dad0f29e0c07e5bf30f3
Paulina Sophia Gerchuk <paulinagerchuk@Paulinas-MacBook-Air-3.local>
2023-02-21 13:29:40 +0000

    added first animation about me

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// adding scroll variable to control css animations
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-  }, false);
+// adding animation class to subheadings when clicked
+
+const aboutMe = document.getElementById("about-me");
+const aboutExp = document.getElementById("about-experience");
+const aboutSkill = document.getElementById("about-skills");
+
+aboutMe.onclick = function() {
+  aboutMe.classList.add("animate-up");
+  aboutExp.classList.add("animate-down");
+  aboutSkill.classList.add("animate-down");
+}
